de35f93df68443c20c09708bda10d204cb08fd69
Surma <surma@surma.dev>
2020-08-08 16:46:25 +0100

    Use bindgen for bindings

--- src/libraw/build.rs
+++ src/libraw/build.rs
@@ -1,5 +1,9 @@
+use std::env;
+use std::path::PathBuf;
 use std::process::Command;
 use std::string::String;
+
+use bindgen;
 
 fn main() {
     let nproc = Command::new("nproc")
@@ -11,4 +15,18 @@ fn main() {
         .args(&[format!("-j{}", nproc).as_str(), "libraw"])
         .output()
         .expect("Failed to build libraw");
+
+    println!("cargo:rustc-link-lib=static=raw");
+    println!("cargo:rustc-link-search=.tmp/libraw/.build/lib/.libs");
+
+    let bindings = bindgen::Builder::default()
+        .header(".tmp/libraw/libraw/libraw.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Could not generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- src/libraw/src/main.rs
+++ src/libraw/src/main.rs
@@ -1,3 +1,9 @@
+mod bindings;
+
+use bindings::*;
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "Hello, world! v{}.{}.{}",
+        LIBRAW_MAJOR_VERSION, LIBRAW_MINOR_VERSION, LIBRAW_PATCH_VERSION
+    );
 }
