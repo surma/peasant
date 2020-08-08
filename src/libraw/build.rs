19c1da0b62e02f9334325f181445cddb3c65b72e
Surma <surma@surma.dev>
2020-08-08 16:29:50 +0100

    Parallelize make

--- src/libraw/build.rs
+++ src/libraw/build.rs
@@ -1,8 +1,14 @@
 use std::process::Command;
+use std::string::String;
 
 fn main() {
+    let nproc = Command::new("nproc")
+        .output()
+        .ok()
+        .and_then(|output| String::from_utf8(output.stdout).ok())
+        .unwrap_or(String::from("0"));
     Command::new("make")
-        .args(&["libraw"])
+        .args(&[format!("-j{}", nproc).as_str(), "libraw"])
         .output()
         .expect("Failed to build libraw");
 }
