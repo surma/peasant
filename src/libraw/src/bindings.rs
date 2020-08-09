63ba6ab8625f69754ba3ef7dee19c5873535dd53
Surma <surma@surma.dev>
2020-08-09 12:11:56 +0100

    Suppress warnings

--- src/libraw/src/bindings.rs
+++ src/libraw/src/bindings.rs
@@ -1,6 +1,8 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(improper_ctypes)]
+#![allow(dead_code)]
 
 // Generated during build by `build.rs`
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
