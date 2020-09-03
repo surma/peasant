c7b8fd632b27d766ee1cbc5a243effb2de62f762
Surma <surma@surma.dev>
2020-09-03 17:11:49 +0100

    Forgot about dimensions

--- pixlproc/src/lib.rs
+++ pixlproc/src/lib.rs
@@ -1,54 +1,46 @@
-use std::iter::FromIterator;
-use std::ops::{Deref, DerefMut};
-
 #[derive(Debug, PartialEq)]
-pub struct RGB(Vec<f64>);
+pub struct RGB {
+    width: usize,
+    height: usize,
+    data: Vec<f64>,
+}
 
-impl Deref for RGB {
-    type Target = Vec<f64>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl RGB {
+    fn from_u8<T: IntoIterator<Item = u8>>(width: usize, height: usize, v: T) -> RGB {
+        RGB {
+            width,
+            height,
+            data: v.into_iter().map(|v| (v as f64) / 255.0).collect(),
+        }
+    }
+
+    fn from_u16<T: IntoIterator<Item = u16>>(width: usize, height: usize, v: T) -> RGB {
+        RGB {
+            width,
+            height,
+            data: v.into_iter().map(|v| (v as f64) / 65535.0).collect(),
+        }
+    }
+
+    fn data(&self) -> &[f64] {
+        &self.data
     }
 }
 
-impl DerefMut for RGB {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
+pub trait Pipeline {
+    fn process(input: Vec<RGB>) -> Vec<RGB>;
+    fn clear_caches() {}
 }
-
-impl FromIterator<f64> for RGB {
-    fn from_iter<T: IntoIterator<Item = f64>>(v: T) -> RGB {
-        RGB(v.into_iter().collect())
-    }
-}
-
-impl FromIterator<u8> for RGB {
-    fn from_iter<T: IntoIterator<Item = u8>>(v: T) -> RGB {
-        RGB(v.into_iter().map(|v| (v as f64) / 255.0).collect())
-    }
-}
-
-impl FromIterator<u16> for RGB {
-    fn from_iter<T: IntoIterator<Item = u8>>(v: T) -> RGB {
-        RGB(v.into_iter().map(|v| (v as f64) / 65535.0).collect())
-    }
-}
-
-pub trait Processor {}
 
 #[cfg(test)]
 mod tests {
     use crate::RGB;
 
     #[test]
-    fn convert_from_u8() {
+    fn from_u8() {
         let data: Vec<u8> = vec![0, 128, 255];
-        let out: RGB = data.into_iter().collect();
-        assert_eq!(
-            out,
-            vec![0.0f64, 128.0 / 255.0, 1.0].into_iter().collect(),
-            "Converting [u8] into RGB"
-        );
+        let out: RGB = RGB::from_u8(1, 1, data);
+        let expected = vec![0.0f64, 128.0 / 255.0, 1.0];
+        assert_eq!(out.data(), expected.as_slice(), "Converting [u8] into RGB");
     }
 }
